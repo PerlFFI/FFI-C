52da86fbd1b279f07ac1c69e33a1e22fa3f105de
Graham Ollis <plicease@cpan.org>
2020-04-29 20:26:05 -0600

    update synopsis

--- examples/synopsis/array.pl
+++ examples/synopsis/array.pl
@@ -20,12 +20,24 @@ my $rect_def = FFI::C::ArrayDef->new(
   ]
 );
 
-my $square = $rect_def->create;
-$square->[0]->x(1.0);
-$square->[0]->y(1.0);
-$square->[1]->x(2.0);
-$square->[1]->y(2.0);
+# create a rectangle using the def's create method
+my $square = $rect_def->create([
+  { x => 1.0, y => 1.0 },
+  { x => 2.0, y => 2.0 },
+]);
 
+printf "[[%d %d][%d %d]]\n",
+  $square->[0]->x, $square->[0]->y,
+  $square->[1]->x, $square->[1]->y;   # [[1 1][2 2]]
+
+# move square by 1 on the x axis
+$square->[$_]->x( $square->[$_]->x + 1 ) for 0..1;
+
+printf "[[%d %d][%d %d]]\n",
+  $square->[0]->x, $square->[0]->y,
+  $square->[1]->x, $square->[1]->y;   # [[2 1][3 2]]
+
+# Create a rectange usingn the generated class
 my $rect = Rectangle->new;
 $rect->[0]->x(1.0);
 $rect->[0]->y(1.0);
